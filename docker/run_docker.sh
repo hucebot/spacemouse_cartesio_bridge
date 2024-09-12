16adbd74640fd4ee37d07e7dec12d517383eacf7
Fabio Amadio <fabioamadio93@gmail.com>
2024-09-12 15:54:08 +0200

    Fixed scaling bug

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -10,4 +10,5 @@ docker run \
             --privileged \
             --volume /tmp/.X11-unix:/tmp/.X11-unix \
             --name opensot_spacemouse \
+            --volume ${HOME}/Workspace/spacemouse_cartesio_bridge/scripts/bridge_node:/home/forest_ws/src/spacemouse_cartesio_bridge/scripts/bridge_node \
             opensot_spacemouse
