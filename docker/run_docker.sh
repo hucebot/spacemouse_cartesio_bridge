5208e4bb65ab452e1ddcf25d1f768ee95ce563a7
Fabio Amadio <fabioamadio93@gmail.com>
2024-09-16 13:38:49 +0200

    Lint run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -10,5 +10,4 @@ docker run \
             --privileged \
             --volume /tmp/.X11-unix:/tmp/.X11-unix \
             --name opensot_spacemouse \
-            --volume ${HOME}/Workspace/spacemouse_cartesio_bridge/scripts/bridge_node:/home/forest_ws/src/spacemouse_cartesio_bridge/scripts/bridge_node \
             opensot_spacemouse
